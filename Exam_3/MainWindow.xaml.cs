ddb6d4156e04c848fd767db6f972fa30be79de72
yehuda81 <44754603+yehuda81@users.noreply.github.com>
2019-09-04 20:24:39 +0300

    Add files via upload

---
+++ Exam_3/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Exam_3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool Click;
+        private MainWindowViewModel vm;       
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.vm = new MainWindowViewModel(); 
+            this.DataContext = this.vm;
+        }
+
+        private void ElpsedEventHandler(object sender, ElapsedEventArgs e)
+        {         
+            
+            Action a = new Action(() => timerTXT.Text = $"{e.SignalTime.Millisecond}");
+            SafeInvoke(a);                                   
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Click = true;            
+            Task.Run(() =>
+            {               
+                Timer timer = new Timer(1);
+                timer.Elapsed += ElpsedEventHandler;
+                timer.Start();
+            });
+            Task.Run(() =>
+            {               
+                string size = vm.ReadUrl2();
+                Action action = new Action(() => SizeBox.Text = size);                
+                SafeInvoke(action);                              
+            });
+        }
+        public void SafeInvoke(Action action)
+        {
+            if (Dispatcher.CheckAccess())
+            {
+                action.Invoke();
+                return;
+            }
+            else
+            {
+                this.Dispatcher.BeginInvoke(action);
+            }
+        }
+    }
+}
---
+++ Exam_3/MainWindowViewModel.cs
@@ -0,0 +1,104 @@
+﻿using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace Exam_3
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {        
+        public string URL { get; set; }        
+        public DelegateCommand MyDelegate { get; set; }
+        private string _size;
+        private bool _ButtonIsEnabled;        
+        public event PropertyChangedEventHandler PropertyChanged;    
+       
+        public bool ButtonIsEnabled
+        {
+            get
+            {
+                return _ButtonIsEnabled;
+            }
+            set
+            {
+                _ButtonIsEnabled = value;
+            }
+        }
+        public string Size
+        {
+            get
+            {
+                return _size;
+            }
+            set
+            {
+                _size = value;
+            }
+        }
+        public MainWindowViewModel()
+        {
+            MyDelegate = new DelegateCommand(ExecuteCommand, CanExecuteMethod);
+            Size = "Please Wait...";
+            ButtonIsEnabled = true;
+        }
+        private bool CanExecuteMethod()
+        {            
+            return true;
+        }        
+        private void ExecuteCommand()
+        {            
+            ButtonIsEnabled = false;
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs("ButtonIsEnabled"));            
+        //Debug.WriteLine(URL);
+        // Option 1 ------------------
+        //ReadUrl(URL);
+        // option 2 ------------------
+        //Task.Run(() =>
+        //    {
+        //        Action action = new Action(() => ReadUrl2(URL));
+        //        Thread.Sleep(2000);
+        //        action.Invoke();                
+        //    });            
+        }        
+        public string ReadUrl2()
+        {
+            string url = URL;
+            WebRequest webRequest = WebRequest.Create(url);
+            WebResponse response = webRequest.GetResponseAsync().Result;
+            using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+            {                
+                string text = reader.ReadToEndAsync().Result;
+                // text.Length == is the length of the result 
+                Size = text.Length.ToString();                            
+            }
+            ButtonIsEnabled = true;           
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs("ButtonIsEnabled"));
+            return Size;
+        }
+
+            private async void ReadUrl(string url)
+        {
+            WebRequest webRequest = WebRequest.Create(url);
+            WebResponse response = await webRequest.GetResponseAsync();
+            using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+            {
+                Thread.Sleep(2000);
+                string text = await reader.ReadToEndAsync();
+                // text.Length == is the length of the result 
+                Size = text.Length.ToString();
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs("Size"));
+            }
+            ButtonIsEnabled = true;
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs("ButtonIsEnabled"));            
+        }
+    }
+}
